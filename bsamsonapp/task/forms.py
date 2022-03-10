b456c70e4dd0fd5a4b14a9e72aba14f96845939e
Basile <basile.samson@epitech.eu>
2022-03-11 00:36:57 +0100

    no task

--- bsamsonapp/task/forms.py
+++ bsamsonapp/task/forms.py
@@ -5,15 +5,15 @@ class TaskForm(forms.ModelForm):
     name = forms.CharField(
         max_length=100,
         help_text="Obligatoire. Et oui sinon comment on sait ce qu'il faut faire gros malin !", 
-        widget=forms.TextInput(attrs={"placeholder": "Nom de la nouvelle tâche"})
+        widget=forms.TextInput(attrs={"placeholder": "Nom de la nouvelle tâche"}),
     )
     description = forms.CharField(
         max_length=500,
         help_text="Facultatif, on peut en rajouter une à n'importe quel moment &#128521;", 
-        widget=forms.TextInput(attrs={"placeholder": "Ajouter une description"})
+        required=False,
+        widget=forms.TextInput(attrs={"placeholder": "Ajouter une description"}),
     )
-    # project = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"placeholder": "Nom du projet associé"}))
 
     class Meta:
         model = Task
-        fields = ['name']
+        fields = ['name', 'description']
